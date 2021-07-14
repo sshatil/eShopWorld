dd92f89aa83677d97f88a39653e476deab1d7e7a
sshatil <mutasimshatil@hotmail.com>
2021-07-15 03:58:27 +0800

    design all products

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,4 +1,4 @@
-import { CircularProgress, Grid } from '@material-ui/core';
+import { CircularProgress } from '@material-ui/core';
 import { useContext } from 'react';
 import { GlobalState } from '../GlobalState';
 import '../styles/Products.scss';
@@ -7,16 +7,13 @@ import ProductItem from './utils/ProductItem';
 const Products = () => {
   const state = useContext(GlobalState);
   const [products] = state.ProductsAPI.products;
+  const [isAdmin] = state.userApi.isAdmin;
   return (
     <>
       <div className="products">
-        <Grid container spacing={3}>
-          {products.map((product) => (
-            <Grid item xs={12} sm={4} md={3} key={product._id}>
-              <ProductItem product={product} />
-            </Grid>
-          ))}
-        </Grid>
+        {products.map((product) => (
+          <ProductItem product={product} isAdmin={isAdmin} />
+        ))}
       </div>
       {products.length === 0 && <CircularProgress />}
     </>
