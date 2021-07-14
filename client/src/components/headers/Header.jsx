51fc8a295e7c72ea4a5d8e60ff8d434c727eb08c
sshatil <mutasimshatil@hotmail.com>
2021-07-15 00:01:51 +0800

    set admin router and user router

--- client/src/components/headers/Header.jsx
+++ client/src/components/headers/Header.jsx
@@ -5,29 +5,61 @@ import '../../styles/Header.scss';
 // import { FaBeer } from 'react-icons/fa';
 
 const Header = () => {
-  const value = useContext(GlobalState);
-  console.log(value);
+  const state = useContext(GlobalState);
+  const [isLoggedIn] = state.userApi.isLoggedIn;
+  const [isAdmin] = state.userApi.isAdmin;
+  console.log(state);
+
+  const adminRouter = () => (
+    <>
+      <li>
+        <Link to="/createProducts">Create Product</Link>
+      </li>
+      <li>
+        <Link to="/category">Categories</Link>
+      </li>
+    </>
+  );
+  const userRouter = () => (
+    <>
+      <li>
+        <Link to="/history">History</Link>
+      </li>
+      <li>
+        <Link to="/">Logout</Link>
+      </li>
+    </>
+  );
   return (
     <header className="header">
       <div className="logo">
         <h1>
-          <Link to="/">Logo</Link>
+          <Link to="/">{isAdmin ? 'Admin' : 'Logo'}</Link>
         </h1>
       </div>
       <ul>
         <li>
-          <Link to="/">Product</Link>
+          <Link to="/">{isAdmin ? 'Product' : 'Shop'}</Link>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
+        {isAdmin && adminRouter}
+        {isLoggedIn ? (
+          userRouter
+        ) : (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
       </ul>
       <div>
-        <h4>
-          <Link to="/cart">
-            Cart <span>0</span>
-          </Link>
-        </h4>
+        {isAdmin ? (
+          ''
+        ) : (
+          <h4>
+            <Link to="/cart">
+              Cart <span>0</span>
+            </Link>
+          </h4>
+        )}
       </div>
     </header>
   );
