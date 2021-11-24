b873f7fb79bfd4a6c562b6e09b7ebc6d3626e2d9
sshatil <mutasimshatil@hotmail.com>
2021-11-24 17:13:11 +0800

    add all routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Pages from "./pages/Pages";
 
 const App = () => {
   return (
-    <div>
-      <h1>eShopWorld</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Pages />
+    </Router>
   );
 };
 
