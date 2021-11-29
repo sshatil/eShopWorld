d8cdb031f1248a09be5ea71ceb2ec4b99e315a5a
sshatil <mutasimshatil@hotmail.com>
2021-11-29 14:23:02 +0800

    add single product details page

--- client/src/pages/products/Products.js
+++ client/src/pages/products/Products.js
@@ -1,16 +1,19 @@
 import React, { useContext } from "react";
 import { GlobalState } from "../../GlobalState";
 import ProductItem from "../utils/ProductItem";
+import styles from "./Products.module.css";
 
 const Products = () => {
   const state = useContext(GlobalState);
   const [products] = state.productsApi.products;
   console.log(products);
   return (
-    <div>
-      {products.map((product) => {
-        return <ProductItem key={product._id} product={product} />;
-      })}
+    <div className={styles.product__section}>
+      <div className={styles.products}>
+        {products.map((product) => {
+          return <ProductItem key={product._id} product={product} />;
+        })}
+      </div>
     </div>
   );
 };
