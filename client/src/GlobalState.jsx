b07497ec8db5289da64aad7d7d4a13f15f213c6e
sshatil <mutasimshatil@hotmail.com>
2021-12-05 17:34:43 +0800

    add refresh token in global state

--- client/src/GlobalState.jsx
+++ client/src/GlobalState.jsx
@@ -1,11 +1,22 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 import ProductsApi from "./api/ProductsApi";
 
 export const GlobalState = createContext();
 
 export const DataProvider = ({ children }) => {
   const [token, setToken] = useState(false);
-  // const [alert, setAlert] = useState({ show: true, msg: "" });
+
+  const refreshToken = async () => {
+    const res = await axios.get("/user/refresh_token");
+    setToken(res.data.accesstoken);
+  };
+  useEffect(() => {
+    const firstLogin = localStorage.getItem("firstLogin");
+    if (firstLogin) {
+      refreshToken();
+    }
+  }, []);
 
   const state = {
     token: [token, setToken],
