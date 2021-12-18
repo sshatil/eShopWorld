e1fe51c0ba95d0225d0c34138aaac721925cbe94
sshatil <mutasimshatil@hotmail.com>
2021-12-18 20:17:37 +0800

    customize productitem page

--- client/src/components/utils/ProductItem.jsx
+++ client/src/components/utils/ProductItem.jsx
@@ -15,17 +15,17 @@ const ProductItem = ({ product }) => {
       <div className="product__details">
         <div className="product__header">
           <h3>{title}</h3>
-          <p className="product__id">id: {product_id}</p>
+          {/* <p className="product__id">id: {product_id}</p> */}
         </div>
-        <h5>Price: $ {price}</h5>
-        <div className="btn">
-          <Link to="#!" onClick={() => addToCart(product)}>
-            <button>Add to Cart</button>
-          </Link>
-          <Link to={`/details/${product._id}`}>
-            <button>View Details</button>
-          </Link>
-        </div>
+      </div>
+      <h5 className="product__price">Price: $ {price}</h5>
+      <div className="btn">
+        <Link to="#!" onClick={() => addToCart(product)}>
+          <button>Add to Cart</button>
+        </Link>
+        <Link to={`/details/${product._id}`}>
+          <button>View Details</button>
+        </Link>
       </div>
     </div>
   );
